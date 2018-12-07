e6b62421e89ee7168b361387c78415264ce73488
Mask <2421697126@qq.com>
2018-12-07 18:17:20 +0800

    2018-12-07
    ElasticSearch测试

---
+++ src/main/java/com/mask/blog/repository/es/EsBlogRepository.java
@@ -0,0 +1,18 @@
+package com.mask.blog.repository.es;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import com.mask.blog.domain.es.EsBlog;
+
+public interface EsBlogRepository extends ElasticsearchRepository<EsBlog, String>{
+	/**
+	 * 	分页查询博客（去重）
+	 * @param title
+	 * @param summary
+	 * @param content
+	 * @return
+	 */
+	Page<EsBlog> findDistinctEsBlogByTitleContainingOrSummaryContainingOrContentContaining(String title, String summary, String content, Pageable pageable);
+}
